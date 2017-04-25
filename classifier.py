1ce15124d6f09670bad9f56f2411e8f81d5571d9
Emmett Neyman <eneyman@seas.upenn.edu>
2017-04-25 17:34:22 -0400

    style fixes

--- classifier.py
+++ classifier.py
@@ -19,98 +19,53 @@ from os import listdir
 data_list = []
 target_list = []
 for f in listdir("training_data"):
-	with open("training_data/" + f, "r") as fp:
-		target = [fp.readline().strip()]
-		for line in fp:
-			line = line.strip(' \t\n')
-			if not line or line[0] == "#" or line[0] == "/" or line[0] == "*" or line[0] == "-" or line[0] == "{" or line[0] == "}":
-				continue
-			else:
-				# print(line)
-				data_list.append(line)
-				target_list.append(target)
+    with open("training_data/" + f, "r") as fp:
+        target = [fp.readline().strip()]
+        for line in fp:
+            line = line.strip(' \t\n')
+            if not line or line[0] == "#"\
+                    or line[0] == "/"\
+                    or line[0] == "*"\
+                    or line[0] == "-"\
+                    or line[0] == "{"\
+                    or line[0] == "}":
+                continue
+            else:
+                data_list.append(line)
+                target_list.append(target)
 
 data_array = np.array(data_list)
 
-# userInput = input('Enter line of code: ')
-
-# X_test = np.array([userInput])
-
-# X_test = np.array(['return alex;', 
-#                    'public class Node implements BSTNode {',
-#                    'formats :: F -> Map.Map x (IO Formatter)',
-#                    'Select Distinct x, y'])   
 
 class classifier():
-	history = {}
-	def __init__(self, targetlist, dataarray):
-		self.mlb = MultiLabelBinarizer()
-		self.Y = self.mlb.fit_transform(targetlist)
-		self.classifier = Pipeline([
-			('vectorizer', CountVectorizer()),
-			('tfidf', TfidfTransformer()),
-			('clf', OneVsRestClassifier(LinearSVC()))])
-		self.classifier.fit(dataarray, self.Y)
 
-	def predictCode(self, xtest):
-		X_test = np.array([xtest])
-		self.predicted = self.classifier.predict(X_test)
-		self.all_labels = self.mlb.inverse_transform(self.predicted)
-		self.history[xtest[0]] = self.all_labels[0]
+    history = {}
 
-	def returnPrediction(self):
-		for item in self.all_labels:
-			if item:
-				item = item[0]
-				item = item.replace(',', '')
-				return item
-			else:
-				return 'Input code is too generic to be matched, srry m8.'
+    def __init__(self, targetlist, dataarray):
+        self.mlb = MultiLabelBinarizer()
+        self.Y = self.mlb.fit_transform(targetlist)
+        self.classifier = Pipeline([
+                        ('vectorizer', CountVectorizer()),
+                        ('tfidf', TfidfTransformer()),
+                        ('clf', OneVsRestClassifier(LinearSVC()))])
+        self.classifier.fit(dataarray, self.Y)
 
-	def __str__(self):
-		print('This is a classifier object, here is the history of searches: ')
-		for i in self.history:
-			print('code: ' + str(i) + ' , result: ' + str(self.history[i][0]))
+    def predictCode(self, xtest):
+        X_test = np.array([xtest])
+        self.predicted = self.classifier.predict(X_test)
+        self.all_labels = self.mlb.inverse_transform(self.predicted)
+        self.history[xtest[0]] = self.all_labels[0]
 
-# print('Welcome to the code classifier!')
-# classifier = classifier(target_list, data_array)
-# userInput = input('Enter line of code: ')
-# X_test = np.array([userInput])
-# classifier.predictCode(X_test)
-# classifier.returnPrediction()
+    def returnPrediction(self):
+        for item in self.all_labels:
+            if item:
+                item = item[0]
+                item = item.replace(',', '')
+                return item
+            else:
+                return 'Input code is too generic to be matched'
 
-# userInput = input('Enter line of code: ')
-# X_test = np.array([userInput])
-# classifier.predictCode(X_test)
-# classifier.returnPrediction()
-
-# classifier.__str__()
-
-
-
-# mlb = MultiLabelBinarizer()
-# Y = mlb.fit_transform(target_list)
-
-# classifier = Pipeline([
-# 	('vectorizer', CountVectorizer()),
-# 	('tfidf', TfidfTransformer()),
-# 	('clf', OneVsRestClassifier(LinearSVC()))])
-
-# #OneVsRestClassifier and MultiOutputClassifier seem to do the same thing
-
-# classifier.fit(data_array, Y)
-# predicted = classifier.predict(X_test)
-# all_labels = mlb.inverse_transform(predicted)
-
-# print(predicted)
-
-# for item in all_labels:
-# 	if item:
-# 		item = item[0]
-# 		item = item.replace(',', '')
-# 		print(item)
-# 	else:
-# 		print('Input code is too generic to be matched, srry m8.')
-
-
-
+    def __str__(self):
+        print('This is a classifier object, here is the history of searches: ')
+        for i in self.history:
+            print('code: ' + str(i) + ' , result: ' + str(self.history[i][0]))
--- frontend.py
+++ frontend.py
@@ -1,27 +1,32 @@
 # Frontend for programming language classifier
 # Built by Alex Matthys and Emmett Neyman
 
-from flask import Flask, render_template, request, redirect, url_for, json, jsonify
+from flask import Flask, render_template, request
 import requests
-import classifier
+import classifier as cl
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     global classify
-    classify = classifier.classifier(classifier.target_list, classifier.data_array)
-    return render_template('index.html', code = "")
+    classify = cl.classifier(cl.target_list, cl.data_array)
+    return render_template('index.html', code="")
 
-@app.route('/', methods = ['POST'])
+
+@app.route('/', methods=['POST'])
 def my_form_post():
+    global classify
     classify.predictCode(request.form['code'])
     result = classify.returnPrediction()
-    return render_template('index.html', code = result)
+    return render_template('index.html', code=result)
+
 
 def main():
     app.debug = True
     app.run()
+
 
 if __name__ == '__main__':
     main()
